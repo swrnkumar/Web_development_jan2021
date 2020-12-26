74bb2e9a0ed56a04dcab312b96edfed28c005f95
swrnkumar <swrnkumar@yahoo.com>
2020-12-26 16:22:17 +0100

    updates

---
+++ python_practice/practice_loops.py
@@ -0,0 +1,14 @@
+for i in [0,1,2,3,4,5]:
+    print(i)
+for i in range(6):
+    print(i)
+
+names = ["Harry", "Larry", "Ron", "Lara", "Sara"]
+
+for name in names:
+    print(name)
+
+name1 = "Harry"
+
+for character in name1:
+    print(character)
