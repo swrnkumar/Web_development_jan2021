d51fa8698e647b0ae5af9fd568396d1ce989d433
swrnkumar <swrnkumar@yahoo.com>
2020-12-26 12:32:41 +0100

    practice lessons python

--- python_practice/practice_lessons.py
+++ python_practice/practice_lessons.py
@@ -1,3 +1,14 @@
 print("Hello world!")
 name = input("Enter your name:")
-print("Hello, " + name)
+print(f"Hello, {name}")
+#conditions
+n = int(input("Enter a number: "))
+if n > 0:
+    print("The number  n  is positive ")
+elif n < 0:
+    print("The number n  is negative")
+else:
+    print("The number is zero")
+#sequences
+nameH = "Harry"
+print(nameH[0])
