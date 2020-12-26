e1368371bfc944961cbcb07c592194607e7b5b6a
swrnkumar <swrnkumar@yahoo.com>
2020-12-26 15:51:47 +0100

    updates practice lessons python

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Web_development_Course_jan2021
 
 Hello this is my first commit.
-I have practice lessons from html, css, python and javascript - course CS50 Edx
+Here are my practice lessons from html, css, python and javascript - course CS50 Edx
--- python_practice/practice_lessons.py
+++ python_practice/practice_lessons.py
@@ -1,6 +1,7 @@
 print("Hello world!")
 name = input("Enter your name:")
 print(f"Hello, {name}")
+
 #conditions
 n = int(input("Enter a number: "))
 if n > 0:
@@ -9,6 +10,28 @@ elif n < 0:
     print("The number n  is negative")
 else:
     print("The number is zero")
-#sequences
+
+#sequences, lists practice
 nameH = "Harry"
 print(nameH[0])
+names1 = ["Harry", "Leo", "Sarah", "Lara"]
+
+names1.append("Matt")
+
+names1.sort()
+
+print(names1)
+
+# practice sets
+
+s = set()
+
+s.add(3)
+s.add(456)
+s.add(-1)
+s.add(3)
+s.remove(3)
+print(s)
+
+print(f"The number of elements in the set is {len(s)} ")
+print(f"The number of elements in the list is {len(names1)}")
