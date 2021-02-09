6f3b6751e4b109d6d181b774887aa9de130a3fe8
swrnkumar <swrnkumar@yahoo.com>
2021-02-09 23:45:09 +0100

    updates to mummys blog

--- Padma_cooking_blog/Recipes/views.py
+++ Padma_cooking_blog/Recipes/views.py
@@ -3,4 +3,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("First recipe")
+    return HttpResponse("Welcome to my blog. This is my first recipe")
