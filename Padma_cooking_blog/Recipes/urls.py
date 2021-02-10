84356b399c768b78c84493089574131100eaf48f
swrnkumar <swrnkumar@yahoo.com>
2021-02-10 18:22:43 +0100

    updates to web development course.django practice

--- Padma_cooking_blog/Recipes/urls.py
+++ Padma_cooking_blog/Recipes/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
-
+    path("", views.index, name="index"),
+    path("<str:name>", views.greet, name="greet"),
+    path("swa", views.swa, name="swa"),
+    path("amb", views.amb, name="amb")
 ]
--- Padma_cooking_blog/Recipes/views.py
+++ Padma_cooking_blog/Recipes/views.py
@@ -3,4 +3,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Welcome to my blog. This is my first recipe")
+    return render(request, "welcome/hello.html")
+
+def swa(request):
+    return HttpResponse("Hi Swa!")
+
+def amb(request):
+    return HttpResponse("Hi AMB!")
+
+def greet(request, name):
+    return render(request, "welcome/greet.html", {
+        "name": name.capitalize()
+    })
